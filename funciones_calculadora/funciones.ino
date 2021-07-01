b4b7a8015467a125df9d9caa1c2cd5a529a16f83
daniel97201020 <67565739+daniel97201020@users.noreply.github.com>
2021-07-01 08:27:15 -0500

    Primer Commir
    Creación  documento colaborativo

---
+++ funciones_calculadora/funciones.ino
@@ -0,0 +1,31 @@
+float suma(int,int);
+float division(int,int);
+float multi(int,int);
+float resta(int,int);
+
+
+void setup(){
+
+}
+
+void loop(){
+
+}
+
+
+float suma(int a, int b){
+
+}
+
+
+float division(int a, int b){
+
+}
+
+float multi(int a, int b){
+
+}
+
+float resta(int a, int b){
+
+}
