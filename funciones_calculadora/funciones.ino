03cf3559666709ae8f10dbd42c728ede932e35f8
Ramenlo <86664981+Ramenlo@users.noreply.github.com>
2021-07-01 08:43:21 -0500

    Cambio de variable y función suma completa.

--- funciones_calculadora/funciones.ino
+++ funciones_calculadora/funciones.ino
@@ -1,7 +1,7 @@
-float suma(int,int);
+long suma(int,int);
 float division(int,int);
-float multi(int,int);
-float resta(int,int);
+long multi(int,int);
+long resta(int,int);
 
 
 void setup(){
@@ -13,19 +13,20 @@ void loop(){
 }
 
 
-float suma(int a, int b){
-
+long suma(int a, int b){
+  long c = 0;
+  c = a+b;
+  return c;
 }
-
 
 float division(int a, int b){
 
 }
 
-float multi(int a, int b){
+long multi(int a, int b){
 
 }
 
-float resta(int a, int b){
+long resta(int a, int b){
 
 }
